d02abda1d0200c871ef864c18a07722da9cf9cfc
Anjana-1234 <anjanaindumini128@gmail.com>
2026-08-27 15:12:12 +0530

    Refactor: update routing logic for admin access and remove AdminLoginPage

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,6 @@ import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import ForgotPasswordPage from './pages/ForgotPasswordPage';
 import AdminPage from './pages/AdminPage';
-import AdminLoginPage from './pages/AdminLoginPage';
 
 function App() {
   return (
@@ -24,35 +23,28 @@ function App() {
       <CartProvider>
         <BrowserRouter>
 
-          <Routes>
+          {/* Navbar shows on every page */}
+          <Navbar />
 
-            {/* Admin routes - no Navbar or Footer */}
-            <Route path="/admin/login" element={<AdminLoginPage />} />
-            <Route path="/admin" element={<AdminPage />} />
+          {/* Main content area */}
+          <main style={{ minHeight: '80vh' }}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/shop" element={<ShopPage />} />
+              <Route path="/gifts" element={<GiftsPage />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/checkout" element={<CheckoutPage />} />
+              <Route path="/order-confirmation/:id" element={<OrderConfirmationPage />} />
+              <Route path="/payment-success" element={<PaymentSuccessPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+              <Route path="/admin" element={<AdminPage />} />
+            </Routes>
+          </main>
 
-            {/* All customer routes - with Navbar and Footer */}
-            <Route path="/*" element={
-              <>
-                <Navbar />
-                <main style={{ minHeight: '80vh' }}>
-                  <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/shop" element={<ShopPage />} />
-                    <Route path="/gifts" element={<GiftsPage />} />
-                    <Route path="/cart" element={<CartPage />} />
-                    <Route path="/checkout" element={<CheckoutPage />} />
-                    <Route path="/order-confirmation/:id" element={<OrderConfirmationPage />} />
-                    <Route path="/payment-success" element={<PaymentSuccessPage />} />
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/signup" element={<SignupPage />} />
-                    <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-                  </Routes>
-                </main>
-                <Footer />
-              </>
-            } />
-
-          </Routes>
+          {/* Footer shows on every page */}
+          <Footer />
 
         </BrowserRouter>
       </CartProvider>
